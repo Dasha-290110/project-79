0c9845bd2a43fe64289e2377ba71f01c9ec8df99
Dasha-290110 <77583048+Dasha-290110@users.noreply.github.com>
2022-06-28 15:32:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-menu_list_array=["Chicken tandoori pizza","deluxe veggie pizza","paneer tikka pizza","veg extravaganza","veg supreme pizza"];
- 
-function getmenu(){
-    var html;
-    htmldata="<ol class='menulist'>"
-    menu_list_array.sort()
- for(var i=0; i <menu_list_array.length;i++){
-     htmldata=htmldata + '(li)'+menu_list_array [i] +'</li>'
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        
     }
- htmldata=html+"(ol)"
- document.getElementById("display_menu").innerHTML=htmldata;
-}
 
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+         menu_list_array.sort()
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
